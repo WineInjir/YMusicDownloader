47c9f06c1061c5716be9e681c0094889cab98f74
WineInjir <gdisk1002@gmail.com>
2025-11-01 09:55:24 +0300

    fixed run.sh

--- run.sh
+++ run.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-#!/bin/bash
-
-ENV_FILE=".env"
-
-if [ ! -f "$ENV_FILE" ]; then
+if [ ! -d ".env" ]; then
     echo "env не найден. Создаю новый..."
     python3 -m venv .env
-    echo env успешно создан"
+    echo "env успешно создан"
 else
     echo "env найден, продолжаю..."
 fi
 
-.venv/bin/pip3 install -r requirements.txt
-.venv/bin/python3 main.py
+.env/bin/pip3 install -r requirements.txt
+.env/bin/python3 main.py

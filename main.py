b14ec2869e14e14d30b8aeb016c191691ec80be2
WineInjir <gdisk1002@gmail.com>
2025-11-01 11:37:21 +0300

    simple downloading

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 print("main.py started, initializing libraries...")
 
 from yandex_music import Client 
-
+from mutagen.mp3 import MP3
+from mutagen.id3 import ID3, TIT2, TPE1, TALB
 
 with open("api.key") as f:
     key = f.read()
 
 print("initializing yandex api")
 client = Client(key).init()
+print("yandex api initialized")
 
-client.users_likes_tracks()[0].fetch_track().download('example.mp3')
+tracks = client.users_likes_tracks()
+for i in range(0, len(tracks)):
+    track = tracks[i].fetchTrack()
+    title = track.title
+    author = track.artists[0].name
+    album = track.albums[0].title
+    print(title, "by", author, "by album" , album)
+    track.download(title + ".mp3", "mp3", 192)
